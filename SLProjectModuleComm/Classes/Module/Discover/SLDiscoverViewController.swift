acc27e8998ab1d461a50df70694b2d44a141294b
2UN7 <1491859758@qq.com>
2021-07-07 17:47:56 +0800

    权限申请

--- SLProjectModuleComm/Classes/Module/Discover/SLDiscoverViewController.swift
+++ SLProjectModuleComm/Classes/Module/Discover/SLDiscoverViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Hero
+import SPPermissions
 
 class SLDiscoverViewController: BaseViewController {
     
@@ -31,7 +32,7 @@ class SLDiscoverViewController: BaseViewController {
         ["友盟分享", "极光分享", "微信支付#######", "支付宝支付########", "App内购########", "融云IM#######"],
         ["网络请求(Moya+RxSwift+HandyJSON)#########", "socket#########"],
         ["组件化调用########", "AOP#########", "字符串插值#########", "自定义操作符#######", "JS交互#######", "WebView########"],
-        ["权限申请#######", "定位########", "手势冲突解决方案#########", "加解密相关#######"],
+        ["权限申请", "定位########", "手势冲突解决方案#########", "加解密相关#######"],
         ["视频播放器#######", "音频播放器#######"]
     ]
 }
@@ -77,6 +78,9 @@ extension SLDiscoverViewController {
         case [0, 1]:
             let vc = ShareViewController(1)
             navigationController?.pushViewController(vc, animated: true)
+        case [3, 0]:
+            let ctr = SPPermissions.list([.reminders, .camera, .photoLibrary, .locationWhenInUse, .locationAlways, .calendar, .bluetooth, .contacts, .microphone])
+            ctr.present(on: self)
         default:
             break
         }
